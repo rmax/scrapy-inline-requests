9badd58a888972ed0dc19b09ec628b8b357dca84
Rolando Espinoza La fuente <darkrho@gmail.com>
2013-02-03 19:13:22 -0400

    removed BOT_* and USER_AGENT from example project to be scrapy 16.x compliant.

--- example/stackoverflow/__init__.py
+++ example/stackoverflow/__init__.py
@@ -14,12 +14,8 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(
 ))))
 
 
-BOT_NAME = 'SOBot'
-BOT_VERSION = '1.0'
-
 SPIDER_MODULES = ['stackoverflow']
 NEWSPIDER_MODULE = 'stackoverflow'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
 CONCURRENT_REQUESTS = 1
 CONCURRENT_ITEMS = 1
