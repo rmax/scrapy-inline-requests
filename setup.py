ce14a47209c892bf1622e6b1b963ea5b2f5158f5
Rolando Espinoza La fuente <darkrho@gmail.com>
2013-02-03 20:19:28 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup
 
-setup(name='scrapy-inline-requests',
-      version='0.1',
-      description='Scrapy decorator for inline requests',
-      author='Rolando Espinoza La fuente',
-      author_email='darkrho@gmail.com',
-      url='https://github.com/darkrho/scrapy-inline-requests',
-      license='BSD',
-      py_modules = ['inline_requests'],
-      install_requires=[
-          'Scrapy>=0.14',
-      ],
-      classifiers=[
-          'Programming Language :: Python',
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-      ],
-     )
+LONG_DESC = open('README.rst').read()
+
+setup(
+    name='scrapy-inline-requests',
+    version='0.1.1',
+    description='Scrapy decorator for inline requests',
+    long_description=LONG_DESC,
+    author='Rolando Espinoza La fuente',
+    author_email='darkrho@gmail.com',
+    url='https://github.com/darkrho/scrapy-inline-requests',
+    license='BSD',
+    py_modules=['inline_requests'],
+    install_requires=[
+        'Scrapy>=0.14',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+    ],
+)
