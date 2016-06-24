e7cdf2d12b0547f2699965aea8252583359c19fe
Rolando Espinoza <darkrho@gmail.com>
2016-06-23 22:49:32 -0300

    Better decorator docstring.

--- src/inline_requests/decorator.py
+++ src/inline_requests/decorator.py
@@ -22,6 +22,43 @@ def _get_args(method_or_func):
 
 
 def inline_requests(method_or_func):
+    """A decorator to use coroutine-like spider callbacks.
+
+    Example:
+
+    .. code:: python
+
+        class MySpider(Spider):
+
+            @inline_callbacks
+            def parse(self, response):
+                next_url = response.urjoin('?next')
+                try:
+                    next_resp = yield Request(next_url)
+                except Exception as e:
+                    self.logger.exception("An error occurred.")
+                    return
+                else:
+                    yield {"next_url": next_resp.url}
+
+
+    You must conform with the following conventions:
+
+    * The decorated method must be a spider method.
+    * The decorated method must use the ``yield`` keyword or return a
+    generator.
+    * The decorated method must accept ``response`` as the first argument.
+    * The decorated method must yield ``Request`` objects without neither
+    ``callback`` nor ``errback`` set.
+
+    If your requests don't come back to the generator try setting the flag to
+    handle all http statuses:
+
+    .. code:: python
+
+                request.meta['handle_httpstatus_all'] = True
+
+    """
     args = _get_args(method_or_func)
     if not args:
         raise TypeError("Function must accept at least one argument.")
